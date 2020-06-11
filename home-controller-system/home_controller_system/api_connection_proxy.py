d01318e203f5c44836429d1be56728c009a1a071
Rishi <34397126+Ri-shi@users.noreply.github.com>
2020-06-11 16:39:26 +0200

    Fixed Api Wrapper Proxies to use boto Resource objects instead of low-level clients

--- home-controller-system/home_controller_system/api_connection_proxy.py
+++ home-controller-system/home_controller_system/api_connection_proxy.py
@@ -1,72 +1,84 @@
 import boto3
-from botocore.exceptions import ClientError
 from abc import ABC
-from decorators import singleton
 
 
 class APIConnectionProxy(ABC):
     def __init__(self, service):
-        self.service = boto3.client(service)
+        self.service = boto3.resource(service)
 
     def data_type(self, obj):
-        if type(obj) == 'str':
+        if type(obj) == str:
             return 'String'
-        if type(obj) == 'int':
+        if type(obj) == int:
             return 'Number'
 
 
 class SQSQueue(APIConnectionProxy):
     def __init__(self, url):
-        super('sqs')
-        self.url = url
+        super().__init__('sqs')
+        self.queue = self.service.Queue(url)
 
     def __convert_dictionary__(self, obj):
         return {key: {'DataType': self.data_type(
-            obj[key]), 'StringValue': obj[key]} for key in obj}
+            obj[key]), 'StringValue': str(obj[key])} for key in obj}
 
-    def send_message(self, messageattributes: dict, messagebody: str):
-        return self.service.send_message(
-            QueueUrl=self.url,
+    def send_message(self, messageattributes: dict, messagebody: str, messagegroupid: str, deduplicationid):
+        return self.queue.send_message(
             MessageAttributes=self.__convert_dictionary__(messageattributes),
-            MessageBody=messagebody
+            MessageBody=messagebody,
+            MessageGroupId=messagegroupid,
+            MessageDeduplicationId=deduplicationid
         )
 
     def recieve_and_delete_message(self, maxnumberofmessages: int = 1, messageattributenames: list = ['All']):
-        response = self.service.recieve_message(
-            QueueUrl=self.url,
+        response = self.queue.receive_messages(
             AttributeNames=['SentTimestamp'],
             MaxNumberOfMessages=maxnumberofmessages,
             MessageAttributeNames=messageattributenames,
-            VisabilityTimeout=0,
+            VisibilityTimeout=0,
             WaitTimeSeconds=0
         )
 
-        message = response['Messages'][0]
-        receipt_handle = message['RecieptHandle']
+        message = response[0]
+        message_id = message.message_id
+        receipt_handle = message.receipt_handle
 
-        self.service.delete_message(
-            QueueUrl=self.url, RecieptHandle=receipt_handle)
+        deleteresponse = self.queue.delete_messages(Entries=[{
+            'Id': message_id,
+            'ReceiptHandle': receipt_handle
+        }])
 
-        return response
+        return message, response, deleteresponse
 
 
 class S3Bucket(APIConnectionProxy):
     def __init__(self, bucket):
-        super('s3')
-        self.bucket = bucket
+        super().__init__('s3')
+        self.bucket = self.service.Bucket(bucket)
 
-    def upload_file(self, metadata, filename_to_upload, object_name=None):
-        if object_name is None:
-            object_name = filename_to_upload
+    def upload_file(self, metadata, filename_to_upload, file_object):
         response = None
+
         try:
-            response = self.service.upload_file(
-                filename_to_upload,
-                self.bucket,
-                object_name,
-                ExtraArgs={'Metadata': metadata}
-            )
-        except ClientError as e:
+            response = self.bucket.put_object(
+                Metadata=metadata, Key=filename_to_upload, Body=file_object)
+        except Exception as e:
             response = e
+
+        return response
+
+    def get_bucket_names(self):
+        return self.service.buckets.all()
+
+    def delete_file(self, filename):
+        response = self.bucket.delete_objects(
+            Delete={
+                'Objects': [
+                    {
+                        'Key': filename
+                    },
+                ]
+            }
+        )
 
         return response
