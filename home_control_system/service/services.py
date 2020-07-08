76a5f9888473f1dbc4d208548b964c677136bbc9
Jonathen Sundy <jonathensundy@gmail.com>
2020-07-09 00:29:26 +0200

    Login integration
    Once the user logs into the HCP, two hidden files are created:
    1. .hash that stores the users that have logged into the HCP on the same computer, and
    2.  .current_user that stores the latest user that has logged into the HCP.
    
    The .hash file is used as a dictionary of users in order to be able to keep track of the HCP ids of the user. Since the HCP_ID is created the first time the user logs into the HCP, that id needs to be persistently stored, otherwise, the HCP will not know which HCP belongs to the user on the machine that is running the HCP. This also accomodates multiuple users being able to log into one machine and not having conflicting HCP ids.
    
    The HCP_id is a sha256 hash of the user's id and the timestamp that the user logged in. This is going to be persistently stored in the DB when the cameras are integrated with the API :+1:

--- home_control_system/service/services.py
+++ home_control_system/service/services.py
@@ -1,13 +1,68 @@
 import requests
 import json
-from warrant.aws_srp import AWSSRP
+from warrant import AWSSRP, Cognito
 import os
+import datetime
+from hashlib import sha256
+
+
+def login(username, password):
+    # authenticate user
+    client_id = "5bl2caob065vqodmm3sobp3k7d"
+    user_pool_id = "eu-west-1_mQ0D78123"
+    try:
+        u = Cognito(client_id=client_id, user_pool_id=user_pool_id, username=username, user_pool_region='eu-west-1')
+        u.authenticate(password=password)
+        user = u.get_user(attr_map={"user_id": "sub"})
+        user_id = user.sub
+    except Exception as e:
+        print("incorrect username or password")
+        return
+    # generate user data for a user that logs into the system for the first time on a specific computer
+    hcp_id = sha256((str(datetime.datetime.now().timestamp()) + user_id).encode('ascii')).hexdigest()
+    user_logged_in_before = False
+    user = {
+        user_id: {
+            'user_id': user_id,
+            "username": username,
+            "hcp_id": hcp_id
+        },
+    }
+    if os.path.exists('.hash'):     # at least one user has logged on this computer before
+        # TODO: encrypt hash file to prevent it being accessible to user as raw text
+        # user_details = sha256(str(user_details).encode()).hexdigest()
+        f = open('.hash', 'r')
+        user_details = json.loads(f.read())
+        f.close()
+        if user_id not in user_details:     # new user logging into the HCP on this computer
+            user_details.update(user)
+        else:
+            user = user_details    # previous user has logged into the HCP on this computer
+            user_logged_in_before = True
+    else:
+        user_details = user     # first user to log into the HCP in this computer
+
+    u = open('.current_user', 'w')  # write the details of the current user logged in on this computer
+    u.write(json.dumps(user[user_id]))
+    u.close()
+
+    if not user_logged_in_before:   # persistently store the new user that logged into the HCP on this computer
+        f = open('.hash', 'w')
+        f.write(json.dumps(user_details))
+        f.close()
+
+
+def get_current_user_details() -> dict:
+    if os.path.exists('.current_user'):
+        f = open('.current_user', 'r')
+        user_details = f.read()
+        user_details = json.loads(user_details)
+        return user_details
 
 
 def get_token():
     client_id = "5bl2caob065vqodmm3sobp3k7d"
     user_pool_id = "eu-west-1_mQ0D78123"
-
     aws = AWSSRP(username='test', password='Test123@', pool_id=user_pool_id,
                  client_id=client_id, pool_region='eu-west-1')
     token = aws.authenticate_user()
@@ -15,20 +70,21 @@ def get_token():
 
 
 def upload_to_s3(path_to_resource, file_name, tag):
+    user_details = get_current_user_details()
+    token = get_token()
     path = f"{path_to_resource}/{file_name}"
     possible_tags = ['detected', 'periodic', 'movement', 'intruder']
     if os.path.exists(path):
         if tag in possible_tags:
             # get S3 url to post image to
             api_endpoint = 'https://aprebrte8g.execute-api.af-south-1.amazonaws.com/beta/storage/upload'
-            uuid = 'demo1'  # TODO: include confidential pyPi to store global variables
-            token = get_token()
             response = requests.post(
                 api_endpoint, params=
                 {
                     "file_name": file_name,
                     "tag": tag,
-                    "user_id": uuid
+                    "user_id": user_details['user_id'],
+                    "camera_id": "2321"
                 },
                 headers={'Authorization': f'TOK:{token}'}
             )
--- home_control_system/tests/test_services.py
+++ home_control_system/tests/test_services.py
@@ -1,51 +1,20 @@
-
-VIDEO_RELAY_QUEUE = "https://sqs.af-south-1.amazonaws.com/878292117449/VideoReceiver.fifo"
-USER_CONTENT_BUCKET = "watchdog.uservideocontent"
-
-# This needs to be changed to use the Service classes
-
-# def test_connect_bucket():
-#     s3 = create_service('S3Bucket', USER_CONTENT_BUCKET)
-#     buckets = s3.get_bucket_names()
-#     names = [bucket.name for bucket in buckets]
-#     assert USER_CONTENT_BUCKET in names
+from service.services import login, get_current_user_details
+from warrant import Cognito
 
 
-# def test_upload_file():
-#     s3 = create_service('S3Bucket', USER_CONTENT_BUCKET)
-#     response = None
-#     with open('tests/test.txt', 'rb') as data:
-#         response = s3.upload_file({}, "test.txt", data)
-#     assert response.key == 'test.txt' and response.bucket_name == USER_CONTENT_BUCKET
+def test_valid_login():
+    client_id = "5bl2caob065vqodmm3sobp3k7d"
+    user_pool_id = "eu-west-1_mQ0D78123"
+    username = 'sundy'
+    password = 'Test123@'
 
+    login(username, password)
+    user_details = get_current_user_details()
 
-# def test_delete_file():
-#     s3 = create_service('S3Bucket', USER_CONTENT_BUCKET)
-#     response = s3.delete_file('test.txt')
-#     assert len(response['Deleted']) != 0
+    u = Cognito(client_id=client_id, user_pool_id=user_pool_id, username=username, user_pool_region='eu-west-1')
+    u.authenticate(password=password)
+    user = u.get_user(attr_map={"user_id": "sub"})
 
-
-# def test_connect_queue():
-#     queue = create_service('SQSQueue', VIDEO_RELAY_QUEUE)
-#     assert queue is not None
-
-
-# def test_send_message():
-#     queue = create_service('SQSQueue', VIDEO_RELAY_QUEUE)
-#     response = queue.send_message(
-#         {"testing": 1}, "this is a test message", "hcp.testing", "unit_test")
-#     assert response is not None
-
-
-# def test_recieve_and_delete_message():
-#     queue = create_service('SQSQueue', VIDEO_RELAY_QUEUE)
-#     message = queue.recieve_and_delete_message()
-#     assert message is not None
-
-
-# test_connect_queue()
-# test_send_message()
-# test_recieve_and_delete_message()
-# test_connect_bucket()
-# test_upload_file()
-# test_delete_file()
+    assert user_details['user_id'] == user.sub
+    assert user_details['username'] == user.username
+    assert user_details['hcp_id'] != 0
